cb39d0af7b8bb1b883b848afa2965b71f55908ab
Provashish Roy <provashishroy95@gmail.com>
2022-03-01 02:08:47 +0600

    added error handler

--- js/script.js
+++ js/script.js
@@ -1,31 +1,84 @@
+//dynamically property change function 
+const dynamicPropertyChange = (value, istrue) => {
+    if (istrue) {
+        document.getElementById('error').style.display = value;
+    } else {
+        document.getElementById('footer').style.position = value;
+    }
+}
+
+//clear previous data...... 
+const clearData = () => {
+    const mainCard = document.getElementById('main-card');
+    mainCard.textContent = '';
+}
+
+
+
 const searchBtn = () => {
     const input = document.getElementById('input-field');
     const inputValue = input.value;
 
+    clearData();
+    dynamicPropertyChange('fixed', false);
+
+    //check input value is null or not
     if (inputValue == '') {
-        console.log('input value is null');
+        dynamicPropertyChange('block', true);
     } else {
-        const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => displayPhones(data.data));
+        dynamicPropertyChange('none', true);
+        loadPhones(inputValue);
+
     }
+    //clear input field...
+    input.value = '';
+}
 
-
+//make url dynamic by input value
+const loadPhones = searchPhones => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchPhones}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhones(data.data));
 }
 
 //display phones.....
-
 const displayPhones = (phones) => {
     //console.log(phones.length);
+    const mainCard = document.getElementById('main-card');
 
+    clearData();
+    //check array have items or not
     if (phones.length == 0) {
-        console.log('Data not found');
+        dynamicPropertyChange('block', true);
     } else {
         phones.forEach(phone => {
-            //console.log(phone);
-            console.log(phone.phone_name);
+            //console.log(phone.slug);
+            //console.log(phone.phone_name);
+            const div = document.createElement('div');
+            div.className = "col-12 col-md-4 col-lg-4 col-xl-4";
+            div.innerHTML = `
+            <div class="card text-center m-auto shadow-lg p-3 mb-5 bg-body border-style" style="width: 18rem;">
+               <img src="${phone.image}" class="img-fluid card-img-top" alt="...">
+               <div class="card-body">
+                <h5 class="card-title fw-bolder">${phone.phone_name}</h5>
+                <p class="card-text">Brand: ${phone.brand}</p>
+                <button onclick="moreDetails('${phone.slug}')" class="btn btn-outline-info">More Details</button>
+               </div>
+            </div>
+            `;
+            mainCard.appendChild(div);
         });
-    }
+        dynamicPropertyChange('relative', false);
 
+    }
+}
+
+
+
+
+//2nd part start here...
+//see more details of phone by slug 
+const moreDetails = (phoneId) => {
+    console.log(phoneId);
 }
