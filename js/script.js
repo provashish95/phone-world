ca830a05d56928cef35055d21d1d70d9a0952865
Provashish Roy <provashishroy95@gmail.com>
2022-02-28 23:20:26 +0600

    fetch data from API

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log('test');
+const searchBtn = () => {
+    const input = document.getElementById('input-field');
+    const inputValue = input.value;
+
+    if (inputValue == '') {
+        console.log('input value is null');
+    } else {
+        const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayPhones(data.data));
+    }
+
+
+}
+
+//display phones.....
+
+const displayPhones = (phones) => {
+    //console.log(phones.length);
+
+    if (phones.length == 0) {
+        console.log('Data not found');
+    } else {
+        phones.forEach(phone => {
+            //console.log(phone);
+            console.log(phone.phone_name);
+        });
+    }
+
+}
